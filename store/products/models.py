aff014e55988d8394d661f0ce4c26eea15208fc4
Dmitriy <swioneart@gmail.com>
2023-05-08 19:54:51 +0300

    db - product and product_category, fixtures

--- store/products/models.py
+++ store/products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    descrption = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products_images')
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f'Продукт: {self.name} / Категория: {self.category.name}'
--- store/store/urls.py
+++ store/store/urls.py
@@ -18,8 +18,15 @@ from django.contrib import admin
 from django.urls import path
 from products.views import index, products
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('', index, name='index'),
     path('products/', products, name='products'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
